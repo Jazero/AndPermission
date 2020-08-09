f8e974d5e41eed3a5e30232c5abcfa89fc0540c9
majunjie <majj@mochasoft.com.cn>
2020-08-09 21:07:32 +0800

    去掉"正在请求权限" 对话框

--- permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
+++ permission/src/main/java/com/yanzhenjie/permission/task/TaskExecutor.java
@@ -15,7 +15,6 @@
  */
 package com.yanzhenjie.permission.task;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.os.AsyncTask;
 
@@ -29,25 +28,25 @@ public abstract class TaskExecutor<T> extends AsyncTask<Void, Void, T> {
 
     private static Executor sExecutor = Executors.newSingleThreadExecutor();
 
-    private Dialog mDialog;
+//    private Dialog mDialog;
 
     public TaskExecutor(Context context) {
-        this.mDialog = new WaitDialog(context);
-        this.mDialog.setCancelable(false);
+//        this.mDialog = new WaitDialog(context);
+//        this.mDialog.setCancelable(false);
     }
 
     @Override
     protected final void onPreExecute() {
-        if (!mDialog.isShowing()) {
-            mDialog.show();
-        }
+//        if (!mDialog.isShowing()) {
+//            mDialog.show();
+//        }
     }
 
     @Override
     protected final void onPostExecute(T t) {
-        if (mDialog.isShowing()) {
-            mDialog.dismiss();
-        }
+//        if (mDialog.isShowing()) {
+//            mDialog.dismiss();
+//        }
         onFinish(t);
     }
 
